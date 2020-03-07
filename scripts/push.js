5b526748d14b7226783347af5a80726f0e6e49c1
Annani Zhu <1945985458@qq.com>
2020-03-07 16:00:17 +0800

    chore: 优化 log 信息

--- scripts/push.js
+++ scripts/push.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer')
 const execa = require('execa')
+const chalk = require('chalk')
 
 const fs = require('fs')
 const path = require('path')
@@ -21,7 +22,7 @@ const path = require('path')
   ])
 
   if (publishType === 'no') {
-    await execa('git', ['push', '--no-verify'])
+    await pushToRemote()
     return
   }
 
@@ -41,8 +42,16 @@ const path = require('path')
   fs.writeFileSync(scriptPath, content)
 
   // 提交修改后的代码
+  console.log(chalk.cyanBright('版本号修订中...'))
   await execa('git', ['add', '.'])
   await execa('git', ['commit', '-m', `release: 脚本发布v${newVersions}`])
   await execa('npm', ['version', publishType])
-  await execa('git', ['push', '--no-verify'])
+  console.log(chalk.greenBright('版本号更新完成'))
+  pushToRemote()
 })()
+
+async function pushToRemote () {
+  console.log(chalk.cyanBright('正在将代码推送到远程...'))
+  await execa('git', ['push', '--no-verify'])
+  console.log(chalk.green('代码推送成功'))
+}
